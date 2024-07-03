9da1563488976fb50df82469b2b79f8287887a45
jeffy14 <jefferson_faria@outlook.com>
2024-07-03 16:30:02 -0300

    Site - Produtos

--- site.php
+++ site.php
@@ -1,13 +1,17 @@
 <?php
 
 use \Hcode\Page;
+use \Hcode\Model\produtc;
 
 $app->get('/', function() {
+
+	$produtcs = produtc::listAll();
     
 	$page = new Hcode\Page();
 
-	$page->setTpl("index");
-
+	$page->setTpl("index",[
+		'produtcs'=>produtc::checkList($produtcs)
+	]);
 });
 
 
